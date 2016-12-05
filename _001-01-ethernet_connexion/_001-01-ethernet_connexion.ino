2ec4bd02a8d59ee1f17c0219f57bac4c67d0d35c
fogim88 <franck.ouriongim@gmail.com>
2016-12-05 14:13:09 +0100

    MAJ

--- _001-01-ethernet_connexion/_001-01-ethernet_connexion.ino
+++ _001-01-ethernet_connexion/_001-01-ethernet_connexion.ino
@@ -1,38 +1,28 @@
-#include <IRremote.h>
-#include <IRremoteInt.h>
-#include <IRremoteTools.h>
-
-#include <SPI.h>
+//#include <SPI.h>
 #include <Ethernet.h>
 
-
-int RECV_PIN = 11;
-const int ledPin =  2;
-
-
+const int ledPin = 13;
 
 byte mac[] = { 0x90, 0xa2, 0xda, 0x00, 0x1a, 0x71 };
 IPAddress ipLocal(192,168,1,201);
 EthernetServer serveurHTTP(80);
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(115200);
-pinMode(ledPin, OUTPUT);
-Ethernet.begin(mac, ipLocal);
-serveurHTTP.begin();
-delay(1000);
-Serial.print("L'adresse IP du shield Ethernet est :" );
-Serial.println(Ethernet.localIP());
-digitalWrite(ledPin, HIGH);
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  Ethernet.begin(mac, ipLocal);
+  serveurHTTP.begin();
+  delay(1000);
+  Serial.print("L'adresse IP du shield Ethernet est :" );
+  Serial.println(Ethernet.localIP());
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   EthernetClient client=serveurHTTP.available();
   if (client) {
+    digitalWrite(ledPin, HIGH);
     Serial.println("----");
     Serial.println("CLient OK");
    }
-   
 }
 
