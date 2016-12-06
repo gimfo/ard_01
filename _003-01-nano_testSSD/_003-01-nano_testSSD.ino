076c879962cf316750f7b37257d26fad36edf04d
fogim88 <franck.ouriongim@gmail.com>
2016-12-06 07:03:48 +0100

    MAJ 003_01

--- _003-01-nano_testSSD/_003-01-nano_testSSD.ino
+++ _003-01-nano_testSSD/_003-01-nano_testSSD.ino
@@ -1,26 +1,20 @@
-int etatInterrupteur;
+int etatI;
+const int pinSSD=12;
+const int pinI=3
 
 void setup() {
-        // Configuration de la broche 3 en tant qu'entree numerique.
-        pinMode(3,INPUT);
-
-        // Configuration de la broche 2 en tant que sortie numerique
-        pinMode(2,OUTPUT);
+        pinMode(pinI,INPUT);
+        pinMode(pinSSD,OUTPUT);
         Serial.begin(115200);
 }
 
 void loop() {
-
-        // Lire l'etat de la broche 3.
-        etatInterrupteur = digitalRead(3);
+        etatInterrupteur = digitalRead(pinI);
         delay(500);
-        Serial.print(etatInterrupteur);
-        // Changer l'etat de la broche 2 selon
-        // l'etat de l'interrupteur
-        if ( etatInterrupteur == LOW ) {
-                digitalWrite(2,HIGH);
-        } else if ( etatInterrupteur == HIGH ) {
-                digitalWrite(2,LOW);
+        Serial.print(etatI);
+        if ( etatI == LOW ) {
+                digitalWrite(pinSSD,HIGH);
+        } else if ( etatI == HIGH ) {
+                digitalWrite(pinSSD,LOW);
         }
-
 }
